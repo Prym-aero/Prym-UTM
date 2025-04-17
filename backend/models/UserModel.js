4ba4c05b3ea6231085d3d50c1485e283e79c6a2a
unknown <softwareprymaerospace@gmail.com>
2025-04-17 15:29:05 +0530

    Complete the UI of the profile page

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     isVerified: { type: Boolean, default: false }, // Tracks if email is verified
     refreshToken: { type: String, default: null }, // Stores the refresh token
     image: { type: String }, //to store the iamge in base64 form
-    phone: {type: String, unique: true, required: true}, //to store the phone number of the user
+    phone: {type: String, unique: true}, //to store the phone number of the user
     country: {type: String}, //to store the country of the user
     city: {type: String}, //to store the city of the user
 }, { timestamps: true });
